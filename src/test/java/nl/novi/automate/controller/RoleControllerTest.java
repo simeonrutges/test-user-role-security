7e7af5ea6e8b9feb95f35029f468564b53b191a0
SRutges <104904365+628HG09@users.noreply.github.com>
2023-05-30 16:37:52 +0200

    unit testen role klaar

--- src/test/java/nl/novi/automate/controller/RoleControllerTest.java
+++ src/test/java/nl/novi/automate/controller/RoleControllerTest.java
@@ -1,14 +1,16 @@
 package nl.novi.automate.controller;
 
+import nl.novi.automate.security.JwtService;
+import nl.novi.automate.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -16,13 +18,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-// Import your own classes here...
 import nl.novi.automate.dto.RoleDto;
 import nl.novi.automate.service.RoleService;
 
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(RoleController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class RoleControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -30,11 +32,18 @@ class RoleControllerTest {
     @MockBean
     private RoleService roleService;
 
+    @MockBean
+    JwtService jwtService;
+
+    @MockBean
+    private UserService userService;
+
     @Test
+//    @WithMockUser(roles = "BESTUURDER")
     public void createRole_shouldReturnRoleString() throws Exception {
         // Arrange
         RoleDto roleDto = new RoleDto();
-        roleDto.setRolename("TestRole");
+        roleDto.rolename = "TestRole";
         String expected = "Done";
 
         // Define behavior for roleService
@@ -53,5 +62,4 @@ class RoleControllerTest {
 
         verify(roleService, times(1)).createRole(any(RoleDto.class));
     }
-
 }
