29662e0feb028f512c2e6ac0dc725b135efac981
SRutges <104904365+628HG09@users.noreply.github.com>
2023-05-30 15:47:08 +0200

    message unit testen toegevoegd

--- src/main/java/nl/novi/automate/controller/MessageController.java
+++ src/main/java/nl/novi/automate/controller/MessageController.java
@@ -17,39 +17,35 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    @GetMapping("")
-    public ResponseEntity<List<MessageDto>> getAllMessages() {
-        List<MessageDto> messages = messageService.getAllMessages();
-        return ResponseEntity.ok(messages);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<MessageDto> getMessageById(@PathVariable Long id) {
-        MessageDto message = messageService.getMessageById(id);
-        return ResponseEntity.ok(message);
-    }
-
     @PostMapping("")
     public ResponseEntity<MessageDto> createMessage(@RequestBody MessageDto messageDTO) {
         MessageDto createdMessage = messageService.createMessage(messageDTO);
         return ResponseEntity.ok(createdMessage);
     }
 
-    @GetMapping("/user/{username}")
-    public ResponseEntity<List<MessageDto>> getMessagesForUser(@PathVariable String username) {
-        List<MessageDto> messages = messageService.getMessagesForUser(username);
-        return ResponseEntity.ok(messages);
-    }
-
-    // Andere endpoints indien nodig (bijv. update, delete)
     @GetMapping("/{sender}/{receiver}")
     public ResponseEntity<List<MessageDto>> getMessagesForConversation(@PathVariable String sender, @PathVariable String receiver) {
         List<MessageDto> messages = messageService.getMessagesForConversation(sender, receiver);
         return ResponseEntity.ok(messages);
     }
 
+    //    @GetMapping("")
+//    public ResponseEntity<List<MessageDto>> getAllMessages() {
+//        List<MessageDto> messages = messageService.getAllMessages();
+//        return ResponseEntity.ok(messages);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<MessageDto> getMessageById(@PathVariable Long id) {
+//        MessageDto message = messageService.getMessageById(id);
+//        return ResponseEntity.ok(message);
+//    }
 
+    //    @GetMapping("/user/{username}")
+//    public ResponseEntity<List<MessageDto>> getMessagesForUser(@PathVariable String username) {
+//        List<MessageDto> messages = messageService.getMessagesForUser(username);
+//        return ResponseEntity.ok(messages);
+//    }
 
-
-
+    // Andere endpoints indien nodig (update, delete,....)
 }
--- src/main/java/nl/novi/automate/service/MessageService.java
+++ src/main/java/nl/novi/automate/service/MessageService.java
@@ -15,57 +15,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//@Service
-//public class MessageService {
-//
-//    private final MessageRepository messageRepository;
-//    private final DtoMapperService dtoMapperService;
-//    private final UserRepository userRepository;
-//
-//    public MessageService(MessageRepository messageRepository, DtoMapperService dtoMapperService, UserRepository userRepository) {  // Voeg DtoMapperService dtoMapperService toe aan de constructor
-//        this.messageRepository = messageRepository;
-//        this.dtoMapperService = dtoMapperService;
-//        this.userRepository = userRepository;
-//    }
-//
-//    public List<MessageDto> getAllMessages() {
-//        return messageRepository.findAll().stream()
-//                .map(dtoMapperService::messageToDto)
-//                .collect(Collectors.toList());
-//    }
-//
-//    public MessageDto getMessageById(Long id) {
-//        Message message = messageRepository.findById(id)
-//                .orElseThrow(() -> new RecordNotFoundException("Message with id " + id + "not found"));
-//        return dtoMapperService.messageToDto(message);
-//    }
-//
-//    public MessageDto createMessage(MessageDto messageDto) {
-//        // Controleer of de sender en receiver bestaan
-//        User sender = userRepository.findByUsername(messageDto.getSenderUsername().username)
-//                .orElseThrow(() -> new RecordNotFoundException("Sender with username " + messageDto.getSenderUsername().getUsername() + " not found"));
-//        User receiver = userRepository.findByUsername(messageDto.getReceiverUsername().username)
-//                .orElseThrow(() -> new RecordNotFoundException("Receiver with username " + messageDto.getReceiverUsername().getUsername() + " not found"));
-//
-//        // Maak de Message en sla deze op
-//        Message message = dtoMapperService.dtoToMessage(messageDto);
-//        Message savedMessage = messageRepository.save(message);
-//
-//        return dtoMapperService.messageToDto(savedMessage);
-//    }
-//
-//
-//    public List<MessageDto> getMessagesForUser(String username) {
-//        return messageRepository.findBySender_UsernameOrReceiver_Username(username, username).stream()
-//                .map(dtoMapperService::messageToDto)
-//                .collect(Collectors.toList());
-//    }
-//    public List<MessageDto> getMessagesForConversation(String sender, String receiver) {
-//        return messageRepository.findBySender_UsernameAndReceiver_Username(sender, receiver).stream()
-//                .map(dtoMapperService::messageToDto)
-//                .collect(Collectors.toList());
-//    }
-
 @Service
 public class MessageService {
 
@@ -81,36 +30,6 @@ public class MessageService {
         this.notificationRepository = notificationRepository;
     }
 
-    public List<MessageDto> getAllMessages() {
-        return messageRepository.findAll().stream()
-                .map(dtoMapperService::messageToDto)
-                .collect(Collectors.toList());
-    }
-
-    public MessageDto getMessageById(Long id) {
-        Message message = messageRepository.findById(id)
-                .orElseThrow(() -> new RecordNotFoundException("Message with id " + id + " not found"));
-        return dtoMapperService.messageToDto(message);
-    }
-
-//    public MessageDto createMessage(MessageDto messageDto) {
-//        // Controleer of de sender en receiver bestaan
-//        User sender = userRepository.findByUsername(messageDto.getSenderUsername())
-//                .orElseThrow(() -> new RecordNotFoundException("Sender with username " + messageDto.getSenderUsername() + " not found"));
-//        User receiver = userRepository.findByUsername(messageDto.getReceiverUsername())
-//                .orElseThrow(() -> new RecordNotFoundException("Receiver with username " + messageDto.getReceiverUsername() + " not found"));
-//
-//        // Maak de Message en sla deze op
-//        Message message = dtoMapperService.dtoToMessage(messageDto);
-//        message.setSenderUsername(sender.getUsername());
-//        message.setReceiverUsername(receiver.getUsername());
-//        message.setTimestamp(LocalDateTime.now());
-//        Message savedMessage = messageRepository.save(message);
-//
-//        return dtoMapperService.messageToDto(savedMessage);
-//    }
-
-//    test 17/5:
 public MessageDto createMessage(MessageDto messageDto) {
     // Controleer of de sender en receiver bestaan
     User sender = userRepository.findByUsername(messageDto.getSenderUsername())
@@ -138,26 +57,6 @@ public MessageDto createMessage(MessageDto messageDto) {
     return dtoMapperService.messageToDto(savedMessage);
 }
 
-
-
-    public List<MessageDto> getMessagesForUser(String username) {
-        return messageRepository.findBySenderUsernameOrReceiverUsername(username, username).stream()
-                .map(dtoMapperService::messageToDto)
-                .collect(Collectors.toList());
-    }
-
-
-
-
-//    public List<MessageDto> getMessagesForConversation(String sender, String receiver) {
-//        return messageRepository
-//                .findAllBySenderUsernameAndReceiverUsernameOrSenderUsernameAndReceiverUsername(sender, receiver, receiver, sender)
-//                .stream()
-//                .map(dtoMapperService::messageToDto)
-//                .collect(Collectors.toList());
-//    }
-
-
     public List<MessageDto> getMessagesForConversation(String sender, String receiver) {
         return messageRepository
                 .findAllBySenderUsernameAndReceiverUsername(sender, receiver)
@@ -165,6 +64,54 @@ public MessageDto createMessage(MessageDto messageDto) {
                 .map(dtoMapperService::messageToDto)
                 .collect(Collectors.toList());
     }
+
+
+////29-5 tijdens test uitgecomm.
+//    public List<MessageDto> getMessagesForUser(String username) {
+//        return messageRepository.findBySenderUsernameOrReceiverUsername(username, username).stream()
+//                .map(dtoMapperService::messageToDto)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public List<MessageDto> getAllMessages() {
+//        return messageRepository.findAll().stream()
+//                .map(dtoMapperService::messageToDto)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public MessageDto getMessageById(Long id) {
+//        Message message = messageRepository.findById(id)
+//                .orElseThrow(() -> new RecordNotFoundException("Message with id " + id + " not found"));
+//        return dtoMapperService.messageToDto(message);
+//    }
+
+
+
+//    ruim voor 19/5 uitgrcomm..":
+    //    public MessageDto createMessage(MessageDto messageDto) {
+//        // Controleer of de sender en receiver bestaan
+//        User sender = userRepository.findByUsername(messageDto.getSenderUsername())
+//                .orElseThrow(() -> new RecordNotFoundException("Sender with username " + messageDto.getSenderUsername() + " not found"));
+//        User receiver = userRepository.findByUsername(messageDto.getReceiverUsername())
+//                .orElseThrow(() -> new RecordNotFoundException("Receiver with username " + messageDto.getReceiverUsername() + " not found"));
+//
+//        // Maak de Message en sla deze op
+//        Message message = dtoMapperService.dtoToMessage(messageDto);
+//        message.setSenderUsername(sender.getUsername());
+//        message.setReceiverUsername(receiver.getUsername());
+//        message.setTimestamp(LocalDateTime.now());
+//        Message savedMessage = messageRepository.save(message);
+//
+//        return dtoMapperService.messageToDto(savedMessage);
+//    }
+    //
+//    public List<MessageDto> getMessagesForConversation(String sender, String receiver) {
+//        return messageRepository
+//                .findAllBySenderUsernameAndReceiverUsernameOrSenderUsernameAndReceiverUsername(sender, receiver, receiver, sender)
+//                .stream()
+//                .map(dtoMapperService::messageToDto)
+//                .collect(Collectors.toList());
+//    }
 
 }
 
